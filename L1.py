f2ebb1a52abb4922ba489a8049d2317f3c4c83c7
derek-is-the-best <60870193+derek-is-the-best@users.noreply.github.com>
2020-02-10 15:39:39 +0800

    跟電腦對話

---
+++ L1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 14:16:58 2020
+
+@author: juliapao
+"""
+
+print("Hello world!")
+print("I'm","Eric")
+print('He is "cool"')
