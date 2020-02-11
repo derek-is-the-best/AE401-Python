6fb3d46760ead2a8bc3f9f5efd45468d04d82f3d
derek-is-the-best <60870193+derek-is-the-best@users.noreply.github.com>
2020-02-11 14:23:35 +0800

    算BMI

---
+++ BMI.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 14:06:42 2020
+
+@author: juliapao
+"""
+
+身高=float(input("height:"))
+體重=float(input("weight:"))
+BMI=(體重//身高**2)
+print("BMI是",BMI)
