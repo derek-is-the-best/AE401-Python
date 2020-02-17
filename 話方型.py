b9d718975059c37558a018b2d4a7fdc81b208285
derek-is-the-best <60870193+derek-is-the-best@users.noreply.github.com>
2020-02-17 15:32:03 +0800

    函式畫正方形

---
+++ 話方型.py
@@ -0,0 +1,15 @@
+import turtle
+turtle123=turtle.Ｔurtle()
+
+
+def drawing(draw):
+    for i in range(4):
+        turtle123.forward(draw)
+        turtle123.right(90)
+drawing(30)
+turtle123.done()
+
+
+
+        
+
