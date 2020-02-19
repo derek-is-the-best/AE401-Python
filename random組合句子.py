b25a4eae2bed960609612b254760c5dbb987c7b9
derek-is-the-best <60870193+derek-is-the-best@users.noreply.github.com>
2020-02-19 15:45:04 +0800

    隨機組句子

---
+++ random組合句子.py
@@ -0,0 +1,10 @@
+import random as rm
+
+name=["sss","yyy","ppp","ooo"]
+noun=["he","she","it","they"]
+verb=["run","jump","play","sleep"]
+n = rm.randint(0,3)
+no = rm.randint(0,3)
+v = rm.randint(0,3)
+
+print(name[n],verb[v],noun[no])
