7d5ec7115455fbab07164804560aa3681e2caa3d
derek-is-the-best <60870193+derek-is-the-best@users.noreply.github.com>
2020-02-19 15:43:46 +0800

    字典字典

---
+++ dic.py
@@ -0,0 +1,31 @@
+d = {'apple':1111,'banana':1212}
+d['mango'] = '111'
+aaa = d.pop('apple')
+   
+for value in d.values():
+    print (value)
+for key in d.keys():
+    print(key)
+
+#初始化
+#dic = {}
+#dic = {'apple' : 10, 'orange' : 20}
+
+#新增
+#dic['banana'] = 5
+#dic['apple'] = 5
+
+#print(dic['apple'])
+
+#刪除
+#dic.pop('apple')
+
+#印出
+#for k in dic.keys():
+    #print(k)
+
+#for v in dic.values():
+    #print(v)
+
+#for k, v in dic.items():
+    #print(k, '=>', v)
